a7a0355bdad478c05a89b5604ced726bd8a044ce
alekslawek <aleksanderslawek1@gmail.com>
2022-01-21 14:51:22 +0100

    Singup Login redirect and message finished version

--- src/components/login/AuthInformation.js
+++ src/components/login/AuthInformation.js
@@ -1,54 +1,87 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { showStatusMessage } from "../../store/auth-slice";
+import LoadingSpinner from "../LoadingSpinner";
 
 const AuthInformation = () => {
   const location = useLocation();
   const loggedUserName = useSelector((state) => state.auth.userName);
+  const signUpStatus = useSelector((state) => state.auth.signUpStatus);
+  const loginStatus = useSelector((state) => state.auth.loginStatus);
+  const message = useSelector((state) => state.auth.statusMessage);
+  const dispatch = useDispatch();
+  const resetMessageHandler = () => {
+    dispatch(
+      showStatusMessage({
+        message: "",
+        loginStatus: "waiting",
+        signUpStatus: "waiting",
+      })
+    );
+  };
 
+  const loginSuccesMsg = location.state.action === "Login" && (
+    <article>
+      <div>
+        <p>{loggedUserName}</p>
+        <p>Login was successfull</p>
+        <small>
+          <Link onClick={resetMessageHandler} to="/">
+            Start browsing our store!
+          </Link>
+        </small>
+      </div>
+    </article>
+  );
+  const loginFailedMsg = location.state.action === "Login" && (
+    <article>
+      <div>
+        <p>Login failed{message && `, reason: ${message}`}</p>
+        <small>
+          <Link onClick={resetMessageHandler} to="/user/login">
+            Try logging in again!
+          </Link>
+        </small>
+      </div>
+    </article>
+  );
+  const signUpSuccessMsg = location.state.action === "SignUp" && (
+    <article>
+      <div>
+        <p>Created new account</p>
+        <small>
+          <Link onClick={resetMessageHandler} to="/user/login">
+            You can login here!
+          </Link>
+        </small>
+      </div>
+    </article>
+  );
+
+  const signUpFailedMsg = location.state.action === "SignUp" && (
+    <article>
+      <div>
+        <p>Failed to create an account{message && `, reason: ${message}`}</p>
+        <small>
+          <Link onClick={resetMessageHandler} to="/user/signup">
+            Try again here!
+          </Link>
+        </small>
+      </div>
+    </article>
+  );
   return (
     <div className="container">
-      {loggedUserName
-        ? location.state.action === "Login" && (
-            <article>
-              <div>
-                <p>{loggedUserName}</p>
-                <p>Login was successfull</p>
-                <small>
-                  <Link to="/">Start browsing our store!</Link>
-                </small>
-              </div>
-            </article>
-          )
-        : location.state.action === "Login" && (
-            <article>
-              <div>
-                <p>Login failed</p>
-                <small>
-                  <Link to="/user/login">Try logging in again!</Link>
-                </small>
-              </div>
-            </article>
-          )}
-      {location.state.action === "SignUp" && location.state.success && (
-        <article>
-          <div>
-            <p>Created new account</p>
-            <small>
-              <Link to="/user/login">You can login here!</Link>
-            </small>
-          </div>
-        </article>
+      {loginStatus === "success" && loginSuccesMsg}
+      {loginStatus === "failed" && loginFailedMsg}
+      {location.state.action === "Login" && loginStatus === "waiting" && (
+        <LoadingSpinner />
       )}
-      {location.state.action === "SignUp" && !location.state.success && (
-        <article>
-          <div>
-            <p>Failed to create an account</p>
-            <small>
-              <Link to="/user/signup">Try again here!</Link>
-            </small>
-          </div>
-        </article>
+      {signUpStatus === "success" && signUpSuccessMsg}
+      {signUpStatus === "failed" && signUpFailedMsg}
+      {location.state.action === "SignUp" && signUpStatus === "waiting" && (
+        <LoadingSpinner />
       )}
     </div>
   );
